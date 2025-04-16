e2fd44fe7279689bd5c948377e8ced0ef714d7d7
theleal <lgcortez.ph@gmail.com>
2025-04-16 11:16:53 -0300

    Feat: Begin MVC

---
+++ 1 - ASP.NET Core/DemoMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DemoMVC.Models;
+
+namespace DemoMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DemoMVC.Models.Student> Student { get; set; } = default!;
+    }
+}
