622a52f931647868b8b726787d540b54d47e0073
theleal <lgcortez.ph@gmail.com>
2025-04-16 09:05:19 -0300

    Feat: extension method to map routes and configuring pipeline program.cs

---
+++ 1 - ASP.NET Core/Middlewares/MapGetExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Middlewares
+{
+    public static class MapGetExtensions
+    {
+        public static void MapRoutes(this WebApplication app) 
+        {
+            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/TestingTimeLogMiddleware", () => 
+            { 
+                Thread.Sleep(60000); 
+                return "Time elapsed (60000miliseconds) to test middleware"; 
+            });
+        }
+    }
+}
--- 1 - ASP.NET Core/Middlewares/Program.cs
+++ 1 - ASP.NET Core/Middlewares/Program.cs
@@ -5,7 +5,6 @@ var app = builder.Build();
 
 app.UseMiddleware<LogTimeMiddleware>();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/TestingTimeLogMiddleware", () => { Thread.Sleep(60000); return "Time elapsed (60000miliseconds) to test middleware"; });
+app.MapRoutes();
 
 app.Run();
