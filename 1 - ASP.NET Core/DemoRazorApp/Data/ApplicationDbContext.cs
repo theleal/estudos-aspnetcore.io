75408917316aca3b2ff611b98d2ad2d6abc9ed2a
theleal <lgcortez.ph@gmail.com>
2025-04-16 11:35:19 -0300

    Create razor project

---
+++ 1 - ASP.NET Core/DemoRazorApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DemoRazorApp.Models;
+
+namespace DemoRazorApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DemoRazorApp.Models.Student> Student { get; set; } = default!;
+    }
+}
