68ba3276dcc6364ed6871899cfea382c24c55a00
theleal <lgcortez.ph@gmail.com>
2025-04-21 16:44:18 -0300

    WebAPI with controllers

---
+++ 1 - ASP.NET Core/DemoASPNETCoreWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using DemoASPNETCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoASPNETCoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("GetStudents")]
+        public Student GetStudents()
+        {
+            return new Student
+            {
+                Id = 1,
+                Active = true,
+                DateRegister = DateTime.Now,
+                Email = "demoAPI@gmail.com",
+                Name = "thelal"
+            };
+        }
+    }
+}
---
+++ 1 - ASP.NET Core/DemoASPNETCoreWebAPI/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoASPNETCoreWebAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateRegister { get; set; } = DateTime.Now;
+
+        public bool Active { get; set; } = true;
+    }
+}
